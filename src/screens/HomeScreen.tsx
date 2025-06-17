2ff78c7491e3b6b4877cf2414e4d84ed7e57a9f0
Siddhart12345 <shekharsiddharth583@gmail.com>
2025-06-17 18:15:33 +0530

    added header

---
+++ src/screens/HomeScreen.tsx
@@ -0,0 +1,19 @@
+import { IonContent,  IonPage } from '@ionic/react';
+import './HomeScreen.scss';
+import FilterComponent from '../component/homeComponent/FilterComponent';
+const HomeScreen: React.FC = () => {
+  return (
+    <IonPage>
+      
+      <IonContent fullscreen>
+       <div className= 'home_screen'>
+        <div className='header_section'> <FilterComponent/></div>
+       
+       <div className='body_section'></div>
+       </div>
+      </IonContent>
+    </IonPage>
+  );
+};
+
+export default HomeScreen;
